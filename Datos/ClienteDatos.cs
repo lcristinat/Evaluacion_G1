ca36b2ad67ba380d91b7d00c78b7feae49e28656
neoscofield1984 <grodriguez@uca.edu.ni>
2017-05-02 20:05:28 -0600

    Modificacion Capa de Presentacion
    Se agrego el formulario de agregar cliente

--- Datos/ClienteDatos.cs
+++ Datos/ClienteDatos.cs
@@ -63,6 +63,7 @@ namespace Datos
                 userBD.Cedula = c.Cedula;
                 userBD.Direccion = c.Direccion;
                 userBD.FechaProceso = DateTime.Now;
+                userBD.UsuarioProceso = 1;
                 dc.SaveChanges();
             }
             catch (Exception err)
