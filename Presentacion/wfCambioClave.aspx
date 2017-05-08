3b446beed9d607d8e6c08164be6e9fab9258b5e0
marcelo <marcelorod@uca.edu.ni>
2017-05-08 11:04:44 -0600

    Se agrega funcionalidad para inicio de sesion
    Se agrega login, cambio de clave y reporte de usuarios

---
+++ Presentacion/wfCambioClave.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="wfCambioClave.aspx.cs" Inherits="Presentacion.wfCambioClave" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+      <div class="col-md-4 col-md-offset-4">
+        <div class="panel panel-default">
+            <div class="panel-heading">Cambio de Contraseña</div>
+            <div class="panel-body">
+                <div class="col-md-12">
+                    <div class="form-group">
+                        <label class="control-label">Nueva Contraseña</label>
+                        <asp:RequiredFieldValidator ID="rfvLogin" runat="server" ErrorMessage="Campo Requerido" ControlToValidate="txtNuevoPassword" Font-Size="X-Small" ForeColor="Red"></asp:RequiredFieldValidator>
+                        <asp:TextBox ID="txtNuevoPassword" runat="server" class="form-control" placeholder="Nueva Contraseña" TextMode="Password"></asp:TextBox>
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <div class="form-group">
+                        <label class="control-label">Confirmar Contraseña</label>
+                        <asp:RequiredFieldValidator ID="rfvPassword" runat="server" ErrorMessage="Campo Requerido" ControlToValidate="txtPasswordConf" Font-Size="X-Small" ForeColor="Red"></asp:RequiredFieldValidator>
+                        <asp:TextBox ID="txtPasswordConf" runat="server" class="form-control" placeholder="Confirme Contaseña" TextMode="Password"></asp:TextBox>
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <div class="form-group">
+                        <asp:Label ID="lblMensaje" runat="server" Text="" ForeColor="red"></asp:Label>
+                        <asp:CustomValidator ID="cvError" runat="server" ErrorMessage="" ForeColor="red"></asp:CustomValidator>
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <div class="form-group">
+                        <asp:Button ID="txtEntrar" runat="server" Text="Guardar" class="btn btn-info" Width="100%" Font-Bold="True" OnClick="txtEntrar_Click" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ Presentacion/wfLogin.aspx.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Presentacion
+{
+    public partial class wfLogin : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void txtEntrar_Click(object sender, EventArgs e)
+        {
+            string login = txtLogin.Text.ToUpper().Trim(), password = txtPassword.Text.ToUpper().Trim();
+            Negocio.UsuarioNegocio dc = new Negocio.UsuarioNegocio();
+            try
+            {
+                int existeUsuario = dc.existeUsuario(login);
+
+                switch (existeUsuario)
+                {
+                    //usuario no existe
+                    case 0:
+                        lblMensaje.Text = lblMensaje.Text = "Usuario no existe, favor verifique";
+                        break;
+                    //si existe se validan que los datos sean correctos
+                    case 1:
+                        evaluaDatos(password, login);
+                        break;
+                    //usuario esta inactivo
+                    case 3:
+                        lblMensaje.Text = "Usuario se encuentra inactivo";
+                        break;
+                    default:
+                        break;
+                }
+            }
+            catch (Exception err)
+            {
+                cvError.IsValid = false;
+                cvError.Text = "Ocurrio un error, favor verifique*** ERR:" + err.Message;
+            }
+            
+            
+        }
+
+        public void evaluaDatos(string password, string login)
+        {
+            Negocio.UsuarioNegocio dc = new Negocio.UsuarioNegocio();
+            //evaluamos si la contraseña no es la generica
+            try
+            {
+                if (password != "EVALUACION")
+                {
+                    string passEncriptado = dc.CreateMD5(password);
+                    if (dc.validaDatos(login, passEncriptado) == 1)
+                    {
+                        Session.Add("sessionIDUsuario", dc.devolverID(login));
+                        Response.Redirect("Default.aspx");
+                    }
+                    else
+                    {
+                        lblMensaje.Text = "Usuario o contraseña incorrectos";
+                    }
+                }
+                else
+                {
+                    Session.Add("sessionIDUsuario", dc.devolverID(login));
+                    Response.Redirect("wfCambioClave.aspx");
+                }
+            }
+            catch (Exception err)
+            {
+                cvError.IsValid = false;
+                cvError.Text = "Ocurrio un error, favor verifique*** ERR:" + err.Message;
+            }
+           
+        }
+    }
+}
